6bec30f9ed2ee2e17435d294cfaf1b6bfdb1c73a
Marinus Klasen <marinus@mklasen.nl>
2019-03-18 16:03:34 +0100

    Check if autoload file is readable

--- webp-for-woocommerce.php
+++ webp-for-woocommerce.php
@@ -41,6 +41,11 @@ if ( ! defined( 'WPINC' ) ) {
 /**
  * Require all files with composer.
  */
-require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
+$autoload_file = plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
+if ( is_readable( $autoload_file ) ) {
+	require $autoload_file;
+}
 
 new Radish_WebP\Replace_Images();
